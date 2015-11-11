18c03efb5df43f7f81e47bbf46e8597648f4841f
Sebastian <sebastian@sebastians-imac.local>
2015-11-11 20:22:20 -0300

    player woking, background player not

--- PlayerViewController.swift
+++ PlayerViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import LNPopupController
 import XCDYouTubeKit
+import AVKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
     
@@ -16,23 +18,78 @@ class PlayerViewController: UIViewController {
     var channelTitle: String!
     var videoId: String!
     
-    @IBOutlet weak var playerView: UIView!
+    var player: AVPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //for backround audio
+        try! AVAudioSession.sharedInstance().setActive(true)
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
         
         
         popupItem.title = videoTitle
         popupItem.subtitle = channelTitle
         
-        //get thumb using api, directly using http://img./youtubeid/..  for kingfisher handling or with hcyoutubeparser?
-
+        //get thumb using api, directly using http://img./youtubeid/..  for kingfisher handling or with hcyoutubeparser
         
-        let vc = XCDYouTubeVideoPlayerViewController(videoIdentifier: videoId)
-        vc.presentInView(playerView)
-        vc.moviePlayer.play()
+        XCDYouTubeClient.defaultClient().getVideoWithIdentifier(videoId) { (video, error) -> Void in
+            
+            let url = video!.streamURLs[18] as! NSURL
+
+            self.player = AVPlayer(URL: url)
+            let playerLayer = AVPlayerLayer(player: self.player)
+            playerLayer.frame = self.view.bounds
+            self.view.layer.addSublayer(playerLayer)
+            self.player.play()
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"applicationDidEnterBackground:", name: UIApplicationDidEnterBackgroundNotification, object: nil)
+        
+    }
+    
+    //for backround audio
+    
+    override func viewWillAppear(animated: Bool) {
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+        self.becomeFirstResponder()
     }
 
+    
+    override func viewWillDisappear(animated: Bool) {
+        player.pause()
+        UIApplication.sharedApplication().endReceivingRemoteControlEvents()
+        self.resignFirstResponder()
+    }
+    
+    override func remoteControlReceivedWithEvent(event: UIEvent?) {
+        switch event!.subtype {
+            
+        case .RemoteControlTogglePlayPause:
+            
+            if player.rate == 0 {
+                player.play()
+            } else {
+                player.pause()
+            }
+            break
+        case .RemoteControlPlay:
+            player.play()
+            break
+        case .RemoteControlPause:
+            player.pause()
+            break
+        default:
+            break
+        }
+    }
+
+    func applicationDidEnterBackground(notification: NSNotification) {
+        player.performSelector("play", withObject: nil, afterDelay: 0.01)
+    }
+    
+    func play() {
+        player.play()
+    }
 
 }
